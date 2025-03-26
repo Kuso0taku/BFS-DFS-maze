11aed7c38c93bc87991a932ba8d932d102081c64
Kuso0taku <denustuzan@denustuzan.com>
2025-03-26 19:11:06 +0700

    (cpp) BFS works

--- cpp/FSalgorithms.cpp
+++ cpp/FSalgorithms.cpp
@@ -1,42 +1,54 @@
 #include "FSalgorithms.h"
 #include <queue>
 #include <vector>
-#include <set>
 #include <utility> // для std::pair
 using namespace std;
 
 vector<pair<unsigned int, unsigned int>> Neighbours(pair<unsigned int, unsigned int> current, vector<vector<unsigned short>> &maze) {
   vector<pair<unsigned int, unsigned int>> neighbours;
-  unsigned int columns = static_cast<unsigned int>(maze.size());
-  unsigned int rows = static_cast<unsigned int>(maze[0].size());
+  unsigned int rows = maze.size(); 
+  unsigned int cols = maze[0].size();
   auto i = current.first, j = current.second;
-  if (i != 0 && j != 0) {neighbours.insert(neighbours.end(),{{i-1, j-1}, {i-1, j}, {i, j-1}});}
-  if (i < columns - 1 && j < rows - 1) {neighbours.insert(neighbours.end(), {{i+1, j+1}, {i+1, j}, {i, j+1}});}
-  if (i != 0 && j < rows - 1) {neighbours.insert(neighbours.end(), {{i-1, j+1}});}
-  if (i < columns - 1 && j != 0) {neighbours.insert(neighbours.end(), {{i+1, j-1}});}
+  
+  if (i > 0 && maze[i-1][j] == 0) {neighbours.push_back({i-1, j});}
+  if (i < rows - 1 && maze[i+1][j] == 0) {neighbours.push_back({i+1, j});}
+  if (j > 0 && maze[i][j-1] == 0) {neighbours.push_back({i, j-1});}
+  if (j < cols - 1 && maze[i][j+1] == 0) {neighbours.push_back({i, j+1});}
+
   return neighbours;
 }
 
 int BFS(pair<unsigned int, unsigned int> start, pair<unsigned int, unsigned int> end, vector<vector<unsigned short>> &maze) {
+  if (maze.empty() || maze[0].empty()) {return 0;}
   queue<pair<unsigned int, unsigned int>> queue;
-  set<pair<unsigned int, unsigned int>> visited; // уникальные точки, которые были уже посещены
-  set<pair<unsigned int, unsigned int>> inQueue; // сет для проверки, есть ли в очереди точки
-  
+  vector<vector<pair<unsigned int, unsigned int>>> path;
+  vector<vector<bool>> visited(maze.size(), vector<bool>(maze[0].size(), false)); // с помощью конструктора передаем в visited значения: для maze.size() столбцов в каждую клетку передаем вектор булевых значений размером maze[0].size() колонок со значениями false;
   queue.push(start);
-  inQueue.insert(start);
+  visited[start.first][start.second] = true;
+
+  path.resize(maze.size());
+  for (auto &row : path) {row.resize(maze[0].size());}
+  path[start.first][start.second] = start;
 
   while (!queue.empty()) {
     pair<unsigned int, unsigned int> current = queue.front();
-    if (current == end) {return 1;}
     queue.pop();
-    inQueue.erase(current);
-    visited.insert(current);
-    maze[current.first][current.second] = 2;
+
+    if (current == end) {
+      pair<unsigned int, unsigned int> at = end;
+      while (at != start) {
+        maze[at.first][at.second] = 2;
+        at = path[at.first][at.second];
+      }
+      maze[start.first][start.second] = 2;
+      return 1;
+    }
 
     for (auto neighbour : Neighbours(current, maze)) {
-      if (visited.find(neighbour) == visited.end() && inQueue.find(neighbour) == inQueue.end()) {
+      if (!visited[neighbour.first][neighbour.second]) {
+        visited[neighbour.first][neighbour.second] = true;
         queue.push(neighbour);
-        inQueue.insert(neighbour);
+        path[neighbour.first][neighbour.second] = current;
       }
     }
   }
